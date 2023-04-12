d9c6a83d024ea8ff4963e1fa69e293a8a3a53369
Felix Guenther <felixguenther1@gmx.de>
2023-04-12 22:27:36 +0200

    remove date check

--- code/daily_nowcast_per_strat_postproc.R
+++ code/daily_nowcast_per_strat_postproc.R
@@ -4,12 +4,10 @@ now = max(dat$date)
 max_delay = 35
 per_len = 56
 
-if (now==Sys.Date()) {
-  files = list.files(paste0("../results/daily_results/", now), full.names = T)
-  smry_list = lapply(files, function(x) {summarise_fit(x, 
-                                                       now = now, 
-                                                       start_date = now-per_len+1, 
-                                                       max_delay = max_delay)})
-  write_csv(do.call(rbind, smry_list), 
-            file = paste0("../hospitalization-nowcast-hub/data-processed/SU-hier_bayes/",now,"-SU-hier_bayes.csv"))
-}
+files = list.files(paste0("../results/daily_results/", now), full.names = T)
+smry_list = lapply(files, function(x) {summarise_fit(x, 
+                                                     now = now, 
+                                                     start_date = now-per_len+1, 
+                                                     max_delay = max_delay)})
+write_csv(do.call(rbind, smry_list), 
+          file = paste0("../hospitalization-nowcast-hub/data-processed/SU-hier_bayes/",now,"-SU-hier_bayes.csv"))
